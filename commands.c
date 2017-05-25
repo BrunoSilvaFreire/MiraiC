d6165ce5db702b18e61a5e109990b8f6dea244e8
DDEVIL\bruno <brunorbsf@gmail.com>
2017-05-25 17:23:45 -0300

    - Adicionado introdução e outras coisas basicas

---
+++ commands.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+void helloCommand() {
+    printf("Ola! ^_^");
+}
--- main.c
+++ main.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <conio.h>
+#include "commands.c"
+
+#define BUZZ_SIZE 2048
+
+char *readFace();
+
+void debug(char string[98]);
+
+/**
+ *
+ * @return Uma string com
+ */
+char *getAvailableCommands() {
+    return "hello.";
+}
 
 int main() {
+    //Ler arquivo da face da Mirai
+    char *miraiFace = readFace();
+    printf(miraiFace);
+    printf("\n");
+
+    //Introdução
+    printf("Ola, eu sou a mirai! Eu sou um robo feito para facilitar a sua vida :D\n");
+    printf("Eu venho com varios comandos de utilidade que voce pode utilizar, entre eles:\n");
+    char *comandosDisponiveis = getAvailableCommands();
+    printf(comandosDisponiveis);
     return 0;
+}
+
+/**
+ * Procura por um arquivo que se chame
+ * @return
+ */
+char *readFace() {
+    char buff[BUZZ_SIZE];
+    FILE *f = fopen("mirai.txt", "r");
+    if (f == NULL) {
+        debug("Nao foi possivel encontrar um arquivo com a face de Mirai! Usando face padrao nada interessante.\n");
+        return ":D";
+    }
+    printf(f->_base);
+    fgets(buff, BUZZ_SIZE, f);
+    return buff;
+}
+
+void debug(char *string) {
+    printf("[DEBUG] ");
+    printf(string);
 }
