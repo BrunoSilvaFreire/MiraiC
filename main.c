24604ea82059c961b725fc17ee7509720069add3
Bruno Ribeiro Braga Silva Freire <ribeiro.freire@pucpr.edu.br>
2017-11-21 12:15:04 -0200

    it works

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TOTAL_COMMANDS 8
+#define TOTAL_COMMANDS 10
 #define COMMAND_LIST_HEADER "--------------------------------\n"
 #define QUIT_COMMAND "quit"
 #define HELP_COMMAND "help"
@@ -13,8 +13,34 @@
 #define PRICE_OF_FILLING_AREA_COMMAND "precoPreenchimentoMaterialArea"
 #define PRICE_OF_FILLING_VOLUME_COMMAND "precoPreenchimentoMaterialVolume"
 #define CHILL_COMMAND "desestressar"
+#define SHOW_ACTION_COMMAND "log"
+#define SHOW_ALL_COMMAND "logAll"
 #define LOG_FILE_NAME "log.mirai"
 
+char *AVAILABLE_COMMANDS[TOTAL_COMMANDS] = {
+        QUIT_COMMAND,
+        HELP_COMMAND,
+        HELLO_COMMAND,
+        SQUARE_AREA_COMMAND,
+        SQUARE_VOL_COMMAND,
+        PRICE_OF_FILLING_AREA_COMMAND,
+        PRICE_OF_FILLING_VOLUME_COMMAND,
+        CHILL_COMMAND,
+        SHOW_ACTION_COMMAND,
+        SHOW_ALL_COMMAND
+};
+char *COMMANDS_DESCRIPTIONS[TOTAL_COMMANDS] = {
+        "Termina a execucao de Mirai",
+        "Mostra esta lista de comandos",
+        "Diz ola!",
+        "Calcula a area de um quadrado",
+        "Calcula o volume de um quadrado",
+        "Calcula o preco para preencher uma area com um material",
+        "Calcula o preco para preencher um espaco com um material",
+        "Abre uma porta secreta para o paraiso",
+        "Mostra as ultimas acoes do usuario",
+		"Mostra todas as ultimas acoes do usuario"
+};
 struct actionArgument {
 	char * chars;
 	int length;
@@ -23,49 +49,49 @@ struct actionArgument {
 struct action {
 	long long start;
 	long long end;
-	ActionArgument * arguments;	
+	int command;
 } typedef Action;
 
 Action currentAction;
 Action * lastActions;
 int totalActions;
+
+Action getAction(int index) {
+	return lastActions[index + 1];
+}
 void printAction(Action a) {
-	printf("Action Start: %d" + a.start);
-	printf("Action End: %d" + a.start);
+	printf("Comando executado: %d(%s)\n",a.command, AVAILABLE_COMMANDS[a.command]);
+	//printf("Iniciado em: %d\n", a.start);
+	//printf("Terminado em: %d\n", a.end);
 }
 void writeLastActions() {
-	printf("Done actions\n");
 	FILE * logFile = fopen(LOG_FILE_NAME, "rt+");
 	if (logFile == NULL) {
 		logFile = fopen(LOG_FILE_NAME, "wb");
 	}
 	fwrite(lastActions, sizeof(Action), totalActions, logFile);
 	fclose(logFile);
-	printf("Done actions2\n");
 }
 
 void readLastActions() {
 	FILE * logFile = fopen(LOG_FILE_NAME, "rt+");
 	if (logFile == NULL) {
+		printf("Couldn't find file '%s', skipping load.\n", LOG_FILE_NAME);
 		logFile = fopen(LOG_FILE_NAME, "wb");
 		return;
 	}
+	printf("Loading actions from file '%s'\n", LOG_FILE_NAME);
 	int count;
 	while(fgetc(logFile) != EOF) {
 		count++;
 	}
+	rewind(logFile);
 	int size = sizeof(Action);
 	totalActions = count / size;
+	lastActions = (Action *) malloc(sizeof(Action) * totalActions);
 	fread(lastActions, size, totalActions, logFile);
 	fclose(logFile);
-	printf("Loaded %d actions: ", totalActions);
-	int i;
-	for (i=0; i < totalActions; i++) {
-		printf("Loading action %d\n", i);
-		Action a = *(lastActions +i);
-		printf("Loaded action %d\n", i);
-		printAction(a);
-	}
+	printf("Loaded %d actions.\n", totalActions);
 }
 long long getCurrentTimestamp() {
     struct timeval te; 
@@ -74,16 +100,16 @@ long long getCurrentTimestamp() {
     // printf("milliseconds: %lld\n", milliseconds);
     return milliseconds;
 }
-void ensureCapacity() {
-}
-Action createNewAction() {
+
+Action createNewAction(int id) {
 	Action action = *((Action *) malloc(sizeof(Action)));
 	action.start = getCurrentTimestamp();
+	action.command = id;
 	return action;
 }
 
-void startNewAction() {
-	currentAction = createNewAction();
+void startNewAction(int name) {
+	currentAction = createNewAction(name);
 }
 
 void addToLog(Action action) {
@@ -100,27 +126,6 @@ void finishAction() {
 	writeLastActions();
 }
 
-char *AVAILABLE_COMMANDS[TOTAL_COMMANDS] = {
-        QUIT_COMMAND,
-        HELP_COMMAND,
-        HELLO_COMMAND,
-        SQUARE_AREA_COMMAND,
-        SQUARE_VOL_COMMAND,
-        PRICE_OF_FILLING_AREA_COMMAND,
-        PRICE_OF_FILLING_VOLUME_COMMAND,
-        CHILL_COMMAND
-};
-char *COMMANDS_DESCRIPTIONS[TOTAL_COMMANDS] = {
-        "Termina a execucao de Mirai",
-        "Mostra esta lista de comandos",
-        "Diz ola!",
-        "Calcula a area de um quadrado",
-        "Calcula o volume de um quadrado",
-        "Calcula o preco para preencher uma area com um material",
-        "Calcula o preco para preencher um espaco com um material",
-        "Abre uma porta secreta para o paraiso"
-};
-
 void addFloatArgument(float i) {
 }
 void addIntArgument(int i) {
@@ -128,14 +133,14 @@ void addIntArgument(int i) {
 int readInteger() {
     int i;
     scanf("%d", &i);
-    addIntArgument(i);
+    //addIntArgument(i);
     return i;
 }
 
 float readFloat() {
     float i;
     scanf("%f", &i);
-    addFloatArgument(i);
+    //addFloatArgument(i);
     return i;
 }
 
@@ -205,14 +210,31 @@ void priceOfSquareVolumeCommand() {
     printf("Informe o preco por metro cubico do material.\n");
     float price = readFloat();
     float finalPrice = volumeOfSquare(height, width, length) * price;
-    printf("O preco final sera de %f$\n", finalPrice);
+    printf("O preco final ssera de %f$\n", finalPrice);
 }
 
 void chillCommand() {
     printf("https://www.google.com.br/search?q=cute_kittens&tbm=isch\n");
     printf("Que a paz esteja com vc :)\n");
 }
-
+void logCommand() {
+	printf("Informe o indice da acao. (Total = %d)", totalActions);
+	int index = readInteger();
+	if (index >= totalActions) {
+		printf("Nao foi possivel encontrar uma acao com esse indice, use um numero entre 0 -> %d", totalActions - 1);
+		return;
+	}
+	Action a = getAction(index);
+	printAction(a);
+}
+void logAllCommand() {
+	int i;
+	for(i = 0; i < totalActions ; i++){
+		printf("Action %d/%d\n", i+1, totalActions);
+		printAction(getAction(i));
+		printf("\n");
+	}
+}
 void executeCommand(int cmd) {
     switch (cmd) {
         case 1:
@@ -236,6 +258,12 @@ void executeCommand(int cmd) {
         case 7:
             chillCommand();
             break;
+        case 8:
+            logCommand();
+            break;
+        case 9:
+            logAllCommand();
+            break;
         default:
             printf("Eu nao conheco esse comando :/ tente um dos commando disponiveis:\n");
             printAvailableCommands();
@@ -249,15 +277,16 @@ int main() {
     printf("Digite o numero de um comando para a execucao! :)\n");
     while (true) {
         printAvailableCommands();
-        startNewAction();
         int cmd;
         scanf("%d", &cmd);
         if (cmd == 0) {
             break;
         }
+        startNewAction(cmd);
         executeCommand(cmd);
         finishAction();
     }
+    writeLastActions();
     printf("Sayounara ^_^\n");
     return 0;
 }
